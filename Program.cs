5555f975bee35c2971bd703b43ff3e849681a112
Natnicha24 <natnicha.ant@mail.kmutt.ac.th>
2021-06-13 15:26:16 +0700

    finalnum1 no 1.2.1

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace finalnum1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i, h,number2;
+        
+            int[,] number = new int[3, 3];
+            for (h = 0; h < 3; h++)
+            { 
+              for( i = 0; i < 3; i++)
+              {
+                    Console.Write("{0}{1}:", i,h);
+                  number[i,h]= int.Parse(Console.ReadLine());
+              }
+            }
+            for (h = 0; h < 3; h++)
+            {
+                for (i = 0; i < 3; i++)
+                {
+                    Console.Write(" {0}",number[i, h]);
+                }
+                Console.WriteLine();
+            }
+            number2 = int.Parse(Console.ReadLine());
+          
+            for (h = 0; h < 3; h++)
+            {
+                for (i = 0; i < 3; i++)
+                {
+                    if(number[i, h] != 0)
+                    { 
+                    Console.Write(" {0}", number[i, h]);
+                    }
+                    else
+                    {
+                        number[i, h] = number2;
+                        Console.Write(" {0}", number[i, h]);
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            }
+
+
+        }
+    }
